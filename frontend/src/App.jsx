1fc2b1373502485e22073c67db2e94b1b211d5fa
_cptSkayt_ <lev.beregovoy.mai@gmail.com>
2025-10-09 11:31:45 +0300

    add chapter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './General.css'
+import Chapter from './componentrs/Chapter/chapter.jsx'
 
 function App() {
   return (
     <>
-      <div className="main"></div>
+      <div className="main">
+        <Chapter>Теория</Chapter>
+      </div>
     </>
   )
 }
