4d6063444c4471ed14d75dd8753a614a4ab080ae
dakdolka <andrejdolinov31@gmail.com>
2025-10-09 08:36:27 +0000

    updated backend infrastructure

--- bot/main.py
+++ bot/main.py
@@ -12,15 +12,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 bot = Bot(settings.TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command('start'))
-async def start(message: Message):
-    kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(
-        text="Открыть WebApp",
-        web_app=WebAppInfo(url=settings.WEB_APP_URL)
-    )]])
-    
-    await message.answer("Нажми кнопку, чтобы открыть WebApp:", reply_markup=kb)
-    
+
 async def main():
     await dp.start_polling(bot)
     
