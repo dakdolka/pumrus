333b824ec8fc4286c69b224bd96f4c6a17ce0255
_cptSkayt_ <lev.beregovoy.mai@gmail.com>
2025-10-09 23:38:14 +0300

    bug fix #8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -113,8 +113,8 @@ function App() {
     // Передаём основные цвета Telegram темы
     setVar("text-color", params.text_color);
     setVar("main-color", params.bg_color);
-    setVar("block-color", isDark ? "rgb(50, 50, 50)" : "rgb(150, 150, 150)");
-    setVar("elem-color", isDark ? "rgb(70, 70, 70)" : "rgb(180, 180, 180)");
+    setVar("block-color", isDark ? "rgb(35, 40, 45)" : "rgb(150, 150, 150)");
+    setVar("elem-color", isDark ? "rgb(30, 30, 35)" : "rgb(180, 180, 180)");
   }, []);
 
   return (
