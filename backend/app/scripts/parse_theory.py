bb3c3ba2ecd1f6437e23a3e74d47688b12be7f96
DolinovAK <dolinovak@mai.ru>
2025-10-09 23:00:55 +0300

    added scripts for adding theory and made first info file

---
+++ backend/app/scripts/parse_theory.py
@@ -0,0 +1,73 @@
+from ast import AsyncFunctionDef
+from pathlib import Path
+import os
+import json
+from re import A
+from tkinter import E
+from typing import Text, Optional
+from unittest.mock import Base
+from models import BlockType, Theory, TheoryType, Block
+from core.db import async_engine, async_session_factory
+from sqlalchemy import insert
+import asyncio
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+config = {
+    "1": BlockType.title,
+    "2": BlockType.subtitle,
+    "3": BlockType.rule,
+    "4": BlockType.example,
+    "5": BlockType.exception,
+    "6": BlockType.important,
+    "7": BlockType.text
+}
+
+type_config = {
+    "0": TheoryType.speechpart,
+    "1": TheoryType.text,
+    "2": TheoryType.wordparts,
+    "3": TheoryType.punctuation
+}
+
+async def insert_theory():
+    async with async_session_factory() as session:
+        theory_type = input("Введите тип теории: ")
+        content = input("Введите название теории: ")
+        stmt = insert(Theory).values(type=type_config[theory_type], name=content).returning(Theory.id)
+        result = await session.execute(stmt)
+        new_id = result.scalar_one()
+        await session.commit()
+        return new_id
+        
+async def insert_block(type, content, theory_id):
+        async with async_session_factory() as session:
+            stmt = insert(Block).values(type=type, text=content, theory_id=theory_id).returning(Block.id)
+            result = await session.execute(stmt)
+            new_id = result.scalar_one()
+            await session.commit()
+            return new_id
+        
+
+async def script():
+    file_path = BASE_DIR / 'txts' / input("Введите имя файла: ")
+    with open(file_path, encoding='utf-8') as f:
+        text = f.read()
+    text = text.split('*')
+    theory_id = await insert_theory()
+    for elem in text:
+        elem_type = config[elem[0]]
+        try:
+            elem_content = elem[4:len(elem)-2]
+        except Exception as e:
+            print(e)
+            continue
+        await insert_block(elem_type, elem_content, theory_id)
+
+if __name__ == "__main__":
+    asyncio.run(script())
+    
+    
+    
+
--- backend/main.py
+++ backend/main.py
@@ -1,51 +1,37 @@
-import json
-from pprint import pprint
-from fastapi import Body, FastAPI, HTTPException
-from fastapi.exceptions import RequestValidationError
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import uvicorn
 import asyncio
 import asyncmy
 from contextlib import asynccontextmanager
-# from data import Orm
 from fastapi.middleware.cors import CORSMiddleware
-# from data import Year
-# from app.models import Photo, Points, User_info, Person, Info
-# from config import settings
-# from data import settings
-# from app.bot import send_to_moderation, bot, dp
-from pydantic import BaseModel
-# async def save_images(image: Photo):
-#     # image_base64 = base64.b64encode(image).decode("utf-8")
-#     # image.img_del = resp.json().data.delete_url
 from app.crud.create import create_all
+from app.core.config import settings
 
 
-class MyException(Exception):
-    def __init__(self, status_code: int, message: str):
-        self.status_code = status_code
-        self.message = message
+# class MyException(Exception):
+#     def __init__(self, status_code: int, message: str):
+#         self.status_code = status_code
+#         self.message = message
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # while True:
-    #     try:
-    #         conn = await asyncmy.connect(
-    #             host=settings.host,
-    #             user=settings.user,
-    #             password=settings.password,
-    #             database=settings.db,
-    #             port=int(settings.port)
-    #         )
-    #         await conn.ensure_closed()
-    #         print("MySQL is ready!")
-    #         break
-    #     except Exception as e:
-    #         print("Waiting for MySQL to be ready...", str(e))
-    #         await asyncio.sleep(1)
-    # await Orm.create_all()
-    # asyncio.create_task(dp.start_polling(bot))
+    while True:
+        try:
+            conn = await asyncmy.connect(
+                host=settings.host,
+                user=settings.user,
+                password=settings.password,
+                database=settings.db,
+                port=int(settings.port)
+            )
+            await conn.ensure_closed()
+            print("MySQL is ready!")
+            break
+        except Exception as e:
+            print("Waiting for MySQL to be ready...", str(e))
+            await asyncio.sleep(1)
     await create_all()
     yield
 
