06c9001517ee18ba44985ca8d0f0c9ed02ecd6c0
DolinovAK <dolinovak@mai.ru>
2025-10-10 14:46:52 +0300

    made correct launch

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -1,9 +1,5 @@
 import uvicorn
+from app.api.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "backend.app.main:app",  # путь к FastAPI приложению
-        host="0.0.0.0",
-        port=8000,
-        reload=True  # только для разработки
-    )
+    uvicorn.run(app, host="0.0.0.0", port=8000)
