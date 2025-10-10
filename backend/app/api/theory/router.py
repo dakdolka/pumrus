51846dc445ea417cb0e1d251bb247da7a0e97508
DolinovAK <dolinovak@mai.ru>
2025-10-10 17:14:41 +0300

    fixed some bugs in getting theory endpoint

--- backend/app/api/theory/router.py
+++ backend/app/api/theory/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db import async_session_factory
-from app.core.theory.repository import TheoryRepositoryImpl
-from app.core.theory.use_cases import GetTheoryUseCase
+from app.infra.theory.repository_impl import TheoryRepositoryImpl
+from app.core.theory.use_cases import GetTheoryByIdUseCase
 from .schemas import TheoryResponse
 
 router = APIRouter(prefix="/theory", tags=["Theory"])
@@ -10,8 +10,7 @@ router = APIRouter(prefix="/theory", tags=["Theory"])
 @router.get("/{theory_id}", response_model=TheoryResponse)
 async def get_theory(theory_id: int):
     repo = TheoryRepositoryImpl()
-    usecase = GetTheoryUseCase(repo)
-
+    usecase = GetTheoryByIdUseCase(repo)
     async with async_session_factory() as session:
         theory = await usecase.execute(theory_id)
         if not theory:
