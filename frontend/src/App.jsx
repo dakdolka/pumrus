2cb6eb62c71a9c1c552f9c45f6f27b6827c3cb29
dakdolka <andrejdolinov31@gmail.com>
2025-10-10 19:25:23 +0000

    niiiiice, bug fix in front and back communicatrion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,7 +60,7 @@ function TheoryChoose() {
   const [rules, setRules] = useState(
     []
   ); // * Долэжен быть fetch на получение имен
-  fetch("http://backend:8000/all_theory")
+  fetch("/api/theory/all_theory")
     .then(response => response.json())
     .then(data => {
       console.log(data)
