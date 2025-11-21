883ab743887d8081e3db61e01b38e2065bc42f1b
DolinovAK <dolinovak@mai.ru>
2025-11-21 12:41:06 +0300

    updated parsing script and mest theory

--- backend/app/scripts/create.py
+++ backend/app/scripts/create.py
@@ -1,11 +1,15 @@
 from app.core.db import Base, async_engine, async_session_factory
 from app.infra.theory.models import TheoryBD, TheoryBlockBD
+import asyncio
 
 async def create_all():
         async with async_engine.begin() as conn:
             async_engine.echo = False
             print(Base.metadata.tables.keys())
-            # await conn.run_sync(Base.metadata.drop_all) #TODO убрать в проде
+            await conn.run_sync(Base.metadata.drop_all) #TODO убрать в проде
             await conn.run_sync(Base.metadata.create_all)
             # print('tables created')
             async_engine.echo = True
+            
+if __name__ == "__main__":
+    asyncio.run(create_all())
--- backend/app/scripts/parse_theory.py
+++ backend/app/scripts/parse_theory.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pprint
 import sys
 BASE_DIR = Path(__file__).resolve().parents[2]
 sys.path.insert(0, str(BASE_DIR))
@@ -14,6 +15,7 @@ import os
 
 
 config = {
+    "0": None,
     "1": BlockType.title,
     "2": BlockType.subtitle,
     "3": BlockType.rule,
@@ -35,28 +37,30 @@ type_config = {
 async def script():
     repository = TheoryRepositoryImpl()
     usecase = CreateTheoryUseCase(repository)
-    
     dir_path = BASE_DIR / 'app' / 'scripts' / 'txts'
     for file_name in os.listdir(dir_path):
         file_path = os.path.join(dir_path, file_name)
         with open(file_path, encoding='utf-8') as f:
             text = f.read()
         text = text.split('%')
-        print(text)
-        theory = Theory(
-            id = None,
-            type = type_config[input("Введите тип теории: ")],
-            name = input("Введите название теории: "),
-            blocks = []
-        )
+        theory_type = type_config[text.pop(-1).strip()[-1]]
+        theory_blocks: list[TheoryBlock] = []
         for elem in text:
             if not elem:
                 continue
             elem_type = config[elem[0]]
             elem_content = elem[1:].strip()
-            theory.blocks.append(TheoryBlock(id=None, type=elem_type, content=elem_content, theory_id=theory.id))
+            theory_blocks.append(TheoryBlock(id=None, type=elem_type, content=elem_content, theory_id=None))
+        theory_name = theory_blocks[0].content
+        print(theory_name, theory_type)
+        theory = Theory(
+            id = None,
+            type = theory_type,
+            name = theory_name,
+            blocks = theory_blocks
+        )
         saved_theory = await usecase.execute(theory)
-    return saved_theory
+    return 'Success!'
 
 
 if __name__ == "__main__":
