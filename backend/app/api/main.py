a283589c9cb31da72118206b37c431229d752b92
dakdolka <andrejdolinov31@gmail.com>
2026-01-16 16:36:51 +0000

    finished db staff. smth like prod is now on the server

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 import uvicorn
 import asyncmy
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from app.scripts.create import create_all
-from app.scripts.parse_theory import run_everything
 from app.core.config import settings
 from app.api.theory.router import router as th_rt
 
@@ -27,7 +25,7 @@ async def lifespan(app: FastAPI):
             print("Waiting for MySQL to be ready...", str(e))
             break
     await create_all()
-    await run_everything()
+    # await run_everything() раскомментировать только если ляжет база, и придётся тянуть из файлов
     yield
 
 
--- backend/app/scripts/create.py
+++ backend/app/scripts/create.py
@@ -5,11 +5,8 @@ import asyncio
 async def create_all():
         async with async_engine.begin() as conn:
             async_engine.echo = False
-            print(Base.metadata.tables.keys())
-            await conn.run_sync(Base.metadata.drop_all) #TODO убрать в проде
             await conn.run_sync(Base.metadata.create_all)
-            # print('tables created')
-            # async_engine.echo = True
-            
+            async_engine.echo = True
+
 if __name__ == "__main__":
     asyncio.run(create_all())
