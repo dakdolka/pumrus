192bae7974371ec43f7f027f930553cb332b9a8a
dakdolka <andrejdolinov31@gmail.com>
2026-03-22 12:51:04 +0000

    added vpn for bot precise working

--- bot/main.py
+++ bot/main.py
@@ -1,21 +1,32 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
 from config import settings
 from handlers import all_rts
 
-bot = Bot(
-    token=settings.TOKEN,
-    default=DefaultBotProperties(parse_mode="HTML"),
-)
-dp = Dispatcher()
-
-for router in all_rts:
-    dp.include_router(router)
-
 
 async def main():
-    await dp.start_polling(bot)
+    # AiohttpSession с SOCKS5-прокси
+    session = AiohttpSession(
+        proxy="socks5://host.docker.internal:10808"
+    )
+
+    bot = Bot(
+        token=settings.TOKEN,
+        session=session,
+        default=DefaultBotProperties(parse_mode="HTML"),
+    )
+
+    dp = Dispatcher()
+
+    for router in all_rts:
+        dp.include_router(router)
+
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
 
 
 if __name__ == "__main__":
